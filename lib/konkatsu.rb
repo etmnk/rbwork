a474e9e29b839f106ee4406703fad10bf3fba9dc
etmnk <pref.myu@gmail.com>
2015-01-14 15:57:32 +0900

    add konkatsu

--- lib/konkatsu.rb
+++ lib/konkatsu.rb
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-require 'rspec-power_assert'
-require 'pp'
-MEMBERS = [
+KK_MEMBERS = [
   {name: "A",kibou: ["c","a","b"],sex: "m"},
   {name: "B",kibou: ["c","f","a"],sex: "m"},
   {name: "C",kibou: ["f","c","b"],sex: "m"},
@@ -17,7 +15,7 @@ MEMBERS = [
   {name: "f",kibou: ["D","B","A"],sex: "f"},
 ]
 
-class Members
+class KMembers
   attr_reader :name, :kibou, :sex
   attr_accessor :flag
 
@@ -29,26 +27,37 @@ class Members
   end
 end
 
-member = MEMBERS.each_with_object([]) do |entry, member|
-  member << Members.new(entry)
-end
+class Konkatsu
+	attr_reader :pair
+  def initialize(kk_members)
+    @k_member = kk_members.each_with_object([]) do |entry, k_member|
+      k_member << KMembers.new(entry)
+    end
+    @men = @k_member.select {|member| member.sex == "m"}
+    @women = @k_member.select {|member| member.sex == "f"}
+    @pair = []
+  end
 
-men = member.select {|member| member.sex == "m"}
-women = member.select {|member| member.sex == "f"}
-
-# 第一希望から第三希望まで
-1.upto(3) do |i|
-  men.each do |man|
-    women.each do |woman|
-      if ((man.flag.! && woman.flag.!) &&
-        (woman.kibou[0,i].find {|w_kibou| w_kibou == man.name} &&
-        man.kibou[0,i].find {|m_kibou| m_kibou == woman.name}))
-        man.flag, woman.flag = true, true
-        print man.name , "-", woman.name, "\n"
+  def make_pair
+  	# 第一希望から第三希望まで
+    1.upto(3) do |i|
+      @men.each do |man|
+        @women.each do |woman|
+          if ((man.flag.! && woman.flag.!) &&
+            (woman.kibou[0,i].find {|w_kibou| w_kibou == man.name} &&
+            man.kibou[0,i].find {|m_kibou| m_kibou == woman.name}))
+            man.flag, woman.flag = true, true
+            @pair << man.name + "-" + woman.name
+          end
+        end
       end
     end
   end
 end
+
+#k_member = Konkatsu.new(KK_MEMBERS)
+#k_member.make_pair
+#p k_member.pair
 
 #A-a
 #B-f
