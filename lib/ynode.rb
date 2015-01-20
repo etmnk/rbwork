bd9a5ed8787cd65d99126d00c114241877d0e69c
preface <pref.myu@gmail.com>
2015-01-21 00:56:26 +0900

    modify ynode

--- lib/ynode.rb
+++ lib/ynode.rb
@@ -7,19 +7,31 @@ class YNode
   def initialize(str)
     @str = str.split(//)
     @route = []
-    @route << 'B'
-    @next = 'A'
+    @back = 'B'
+    @route << @back
+    @current = 'A'
 
-    @node = {
-      'A' => {
-        'l' => 'D',
-        'r' => 'C'
-      },
-      'D' => {
-        'l' => 'E',
-        'r' => 'F'
-      }
-    }
+    @node = [
+      %w(B A D C),
+      %w(B C A F),
+      %w(B E F D),
+      %w(A D B F),
+      %w(A C F B),
+      %w(A B C E),
+      %w(D F E C),
+      %w(D E B E),
+      %w(D A C B),
+      %w(C A B D),
+      %w(C B E A),
+      %w(C F D E),
+      %w(E B A C),
+      %w(E D F A),
+      %w(E F C D),
+      %w(F C B A),
+      %w(F E D B),
+      %w(F D A E),
+    ]
+    #h = { 'BA' => {'l'=> 'D','r' => 'C'}}
   end
 
   def get_next
